3eb5ab7c3d48c8cdd79a4f12c68ce2c011c40a7d
Mariana Machado <Mariana1658@users.noreply.github.com>
2019-05-30 17:23:36 -0300

    escala de cores frio-quente removida,  performance_comparison recebe apenas um array
    - todos os gráficos agora seguem a escala de cores branco-azul
    - o método performance_comparison recebe apenas um array e possui uma linha entre diferentes comparações

--- Desempenho_de_busca.py
+++ Desempenho_de_busca.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[18]:
 
 
 import matplotlib.pyplot as plt
@@ -9,6 +9,9 @@ import matplotlib as mpl
 import numpy as np
 import pandas as pd
 import math
+
+import matplotlib.lines as lines
+
 from matplotlib import cm
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 
@@ -72,7 +75,7 @@ def LS_performance(a_array_length): #Linear Search
     
 
 
-# In[19]:
+# In[6]:
 
 
 def g_BS_performance(base, amount, n_array, label): #Binary Search
@@ -159,7 +162,7 @@ def g_BS_performance(base, amount, n_array, label): #Binary Search
     #plt.show() 
 
 
-# In[20]:
+# In[7]:
 
 
 def g_LS_performance(base, amount, n_array, label):  #Linear Search
@@ -169,10 +172,8 @@ def g_LS_performance(base, amount, n_array, label):  #Linear Search
     title = 'linear_search_performance_X' + str(n_array) + '_' + str(base) + '_' + str(base+amount*(n_array-1)) + str(label) + '.png'
     
     #cria um novo mapa de cores
-    colors1 = cm.get_cmap('ocean', 128)
-    colors2 = cm.get_cmap('RdYlGn', 128)
-
-    colors = np.vstack((colors1(np.linspace(1, 0.08, 128)),colors2(np.linspace(1, 0, 128))))
+    colors1 = cm.get_cmap('Blues', 128)
+    colors = np.vstack((colors1(np.linspace(0, 1, 128))))
     cmp = ListedColormap(colors)
     
     #gerencia a distribuição de cores pelo grafico
@@ -240,37 +241,42 @@ def g_LS_performance(base, amount, n_array, label):  #Linear Search
     #plt.show()
 
 
-# In[21]:
+# In[62]:
 
 
-def performance_comparison(a_array_length, a_search_type, label): #Performance Comparison
+def performance_comparison(a_array_length, label): #Performance Comparison
     #Faz uma comparação entre os diferentes tipos de busca
     #A escala de cores tem como base o tamanho do array
     
-    if(len(a_array_length) != len(a_search_type)):
-        return -1
-    
+    title = 'performance_comparison_'
     
     #cria um novo mapa de cores
-    colors1 = cm.get_cmap('ocean', 128)
-    colors2 = cm.get_cmap('RdYlGn', 128)
-
-    colors = np.vstack((colors2(np.linspace(0, 1, 128)),colors1(np.linspace(0.1, 1, 128))))
+    colors1 = cm.get_cmap('Blues', 128)
+    colors = np.vstack((colors1(np.linspace(1, 0, 128))))
     cmp = ListedColormap(colors)
     
     max_size = max(a_array_length)
+    length = len(a_array_length)*2
     scale = 5
     
     if(max_size/10 > 1):
-        fig, axes = plt.subplots(1,len(a_search_type), constrained_layout=False, figsize=(len(a_array_length),max_size/scale))
+        fig, axes = plt.subplots(1,length, constrained_layout=False, figsize=(length,max_size/scale))
     else:
-        fig, axes = plt.subplots(1,len(a_search_type), constrained_layout=False, figsize=(len(a_array_length),3))
+        fig, axes = plt.subplots(1,length, constrained_layout=False, figsize=(length,3))
 
     
-    i=0 #controla o 'a_array_length' e o 'a_search_type'
+    i=0 #controla o 'a_array_length'
+    search_type = 'BS'
+    
     for current_ax in axes:
         
-        if(a_search_type[i]=='BS'): #Binary Search
+        title += search_type
+        title += str(a_array_length[i])
+        title += '_'
+        
+        if(search_type == 'BS'):
+            #Binary Search
+            
             array = BS_performance(a_array_length[i])         
             percent_x = (100*BS_max(a_array_length[i])/a_array_length[i])*0.01 #ajusta a escala de cores
             newcmp = ListedColormap(cmp(np.linspace(1, 1-percent_x, 256)))
@@ -286,36 +292,46 @@ def performance_comparison(a_array_length, a_search_type, label): #Performance C
                 current_ax.set_xticks(np.arange(0, 0, 1.0))
                 current_ax.set_yticks(np.arange(0, 0, 1.0))
             
-        elif(a_search_type[i]=='LS'): #Linear Search
+        elif(search_type=='LS'):
+            #Linear Search
+            
             array = LS_performance(a_array_length[i])
             percent_x = (100/a_array_length[i])/100 #ajusta a escala de cores
-            newcmp = ListedColormap(cmp(np.linspace(percent_x, 1, 256)))
+            newcmp = ListedColormap(cmp(np.linspace(percent_x, 1, 256))) 
             
             if(label):
                 #legenda
                 current_ax.set_yticklabels(array[::-1])
                 current_ax.set_yticks(np.arange(min(array), max(array)+1, 1.0))
                 current_ax.set_xticks(np.arange(0, 0, 1.0))
+                
             else:
                 #set_axis_off()
                 current_ax.set_xticks(np.arange(0, 0, 1.0))
                 current_ax.set_yticks(np.arange(0, 0, 1.0))
-       
+            
         current_ax.pcolormesh(np.row_stack(array),cmap=newcmp)
-        current_ax.set_title(a_search_type[i] + " " + str(a_array_length[i]))    
-        i = i+1
+        current_ax.set_title(search_type + ' ' + str(a_array_length[i]))  
+        
+        if(search_type=='BS'):
+            search_type='LS'
+        elif(search_type=='LS'):
+            search_type='BS'
+            i=i+1
     
-    title = 'performance_comparison_'
-    for i in range(0, len(a_array_length)):
-        title += a_search_type[i]
-        title += str(a_array_length[i])
-        title += '_'
-    
-    plt.tight_layout()
-    
+    #linhas
+    if(len(a_array_length)>1):
+        l_amount = 1/len(a_array_length) 
+        l_position = l_amount
+        for l in range(0, len(a_array_length)):
+            line = lines.Line2D([l_position], [0,1], transform=fig.transFigure, figure=fig, color='#000000')
+            fig.lines.extend([line])
+            l_position = l_position + l_amount
     
     title += str(label)
     title += '.png'
+    
+    plt.tight_layout()
     plt.savefig((title), format='png')
     #plt.show()   
 
@@ -406,27 +422,4 @@ def g_mean_performance(base, amount, g_length, search_type): #Average
     
     plt.savefig((title), format='png')
     #plt.show()
-
-
-# In[22]:
-
-
-g_BS_performance(10, 1, 1, True)
-g_BS_performance(20, 1, 1, True)
-g_BS_performance(10, 10, 5, True)
-g_BS_performance(10, 10, 5, False)
-
-g_LS_performance(10, 1, 1, True)
-g_LS_performance(20, 1, 1, True)
-g_LS_performance(10, 10, 5, True)
-g_LS_performance(10, 10, 5, False)
-
-performance_comparison([40,40,20,20,10,10], ['BS','LS','BS','LS','BS','LS'], True)
-performance_comparison([40,40,20,20,10,10], ['BS','LS','BS','LS','BS','LS'], False)
-
-g_sum_performance(10, 10, 50,'BS')
-g_sum_performance(10, 10, 50,'LS')
-
-g_mean_performance(10, 10, 100,'BS')
-g_mean_performance(10, 10, 100,'LS')
 
